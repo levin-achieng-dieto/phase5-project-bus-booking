9065242ee53627b96f4d1e62e3edd5503fa96091
Essyuge <ugeesther@gmail.com>
2022-12-27 04:06:52 +0300

    dashboard

---
+++ client/src/Data/Data.js
@@ -0,0 +1,38 @@
+import {
+    UilEstate,
+    UilSubway,
+    UilUsersAlt,
+    UilMap,
+    UilDialpad,
+ 
+  } from "@iconscout/react-unicons";
+  
+
+  
+  export const SidebarData = [
+    {
+      icon: UilEstate,
+      heading: "Dashboard",
+    },
+    {
+      icon: UilSubway,
+      heading: "Buses",
+    },
+     {
+      icon: UilMap,
+       heading: "Routes",
+     },
+    {
+      icon: UilDialpad,
+      heading: "Seats"
+    },
+
+    {
+        icon: UilUsersAlt,
+        heading: "Customers",
+    },
+    {
+        icon: UilUsersAlt,
+        heading: "Bookings",
+    },
+]
