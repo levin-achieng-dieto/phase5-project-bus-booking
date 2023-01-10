39d31c12afeeafd97da5e5596ed3957994013311
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2023-01-10 10:05:22 +0300

    adding routes crud

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,2 +1,47 @@
 class RoutesController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :rescue_from_not_found_record
+rescue_from ActiveRecord::RecordInvalid, with:  :rescue_from_invalid_record
+
+    def index
+        render json: Route.all, status: :ok
+    end
+
+    def create
+        route = admin.route.create!(route_params)
+        render json: route, status: :created
+    end
+
+    def update
+        route = Route.find_by(id: params[:id])
+        route.update!(route_params)
+        render json: route, status: :updated
+    end
+
+    def destroy 
+        route = Route.find(params[:id])
+        route.destroy
+        head :no_content
+    end
+
+    def show
+        route = Route.find_by(id: params[:id])
+        render json: route, status: :ok 
+    end
+
+    private
+    def route_params
+        params.permit(:viacities, :busnumber, :date, :time, :cost)
+    end
+
+    def rescue_from_not_found_record
+        render json: {error: "Route not found"}, status: :not_found 
+    end
+
+    def rescue_from_invalid_record(e)
+        render json: {errors: e.record.errors.full_messages}, status: :unprocessable_entity 
+    end
+
+    def admin
+        @admin ||= Admin.find(params[:admin_id])
+    end
 end
