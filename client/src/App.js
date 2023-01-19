c6447292e293dfed2fb5d55ed67971ed0e04ca70
kevin-lomenen <kevin.lomenen@student.moringaschool.com>
2023-01-19 12:46:37 +0300

    all errowr and blocker has been fixed

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Routes from "./Routes/Routes";
+
+const App = () => {
+  return (
+    <div style={{ overflowX: "hidden" }}>
+      <Routes />
+    </div>
+  );
+};
+
+export default App;
