58cabfe8207af461f593b75aa4648ff1999cecca
kevin-lomenen <kevin.lomenen@student.moringaschool.com>
2023-01-20 17:53:58 +0300

    added route selection and and route slector

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Homepage from './components/Homepage/Homepage'
-// import RouteSelection from './components/RouteSelection/RouteSelection'
+import RouteSelection from './components/RouteSelection/RouteSelection'
 // import LogOrsign from './components/Login-Signup/LogOrsign'
 // import Signup from './components/Login-Signup/Signup'
 // import Profile from './components/Profile/Profile'
@@ -15,11 +15,11 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={ <Homepage/> } />
-          {/* <Route path="/login" render={props => <LogOrsign {...props} />} />
-          <Route path="/register" render={props => <Signup {...props} />} />
-          <Route path="/routes" exact render={props => <RouteSelection {...props} />} />
-          <Route path="/profile" exact render={props => <Profile {...props} />} />
-          <Route path="/getTicket" exact render={props => <TicketPage {...props} />} /> */}
+          {/* <Route path="/login" render={props => <LogOrsign {...props} />} /> */}
+          {/* <Route path="/register" render={props => <Signup {...props} />} /> */}
+          <Route path="routes" element={ <RouteSelection/>} />
+          {/* <Route path="/profile" exact render={props => <Profile {...props} />} /> */}
+          {/* <Route path="/getTicket" exact render={props => <TicketPage {...props} />} /> */}
         </Routes>
       </Router>
     </div>
